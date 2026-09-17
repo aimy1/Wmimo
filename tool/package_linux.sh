8d622e62e4de3141c564be3bcb3c58a67e2318b0
im a <aim10086745@gmail.com>
2026-09-17 14:53:44 +0800

    build(linux): update default packaging version to v1.1.1

--- tool/package_linux.sh
+++ tool/package_linux.sh
@@ -19,8 +19,8 @@ fi
 
 RAW_VERSION="${TAG#v}"
 if [ -z "$RAW_VERSION" ] || [ "$RAW_VERSION" = "v" ]; then
-  RAW_VERSION="1.1.0"
-  TAG="v1.1.0"
+  RAW_VERSION="1.1.1"
+  TAG="v1.1.1"
 fi
 VERSION="$RAW_VERSION"
 BUNDLE_DIR="build/linux/x64/release/bundle"
